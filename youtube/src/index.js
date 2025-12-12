d902dea0cb5a8ca3c7673e1d1a7e2bc6721fe7bb
Harsh Mishra <harsh7565023881@gmail.com>
2025-12-13 04:18:23 +0530

    Add youtube/src files (migrated from submodule)

---
+++ youtube/src/index.js
@@ -0,0 +1,38 @@
+// require('dotenv').config({path: './env'})
+                // OR
+import dotenv from "dotenv"
+dotenv.config({
+    path: './env'
+})
+
+
+import connectDB from "./db/index.js";
+
+
+connectDB();
+
+// APPROACH -1 Cnnection of DB 
+
+// import { DB_NAME } from "./constants";
+// import mongoose from "mongoose";
+
+// import express from "express"
+// const app=express()
+
+// // It is a type of function where we connect DB
+// ; (async () => {
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error", () => { //app.on is a listeners of express
+//        console.log("ERRR: ", error);
+//        throw error
+//        })
+        
+//        app.listen(process.env.PORT, () => {
+//         console.log(`App is listening on port ${process.env.PORT}`);
+//        })
+//     }catch(error){
+//        console.error("ERROR: ", error)
+//        throw err
+//     }
+// })()
